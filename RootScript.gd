ba57ae600eb7029efc83b6c4b5eb7c23e406b8b2
sdziatkow <sdziatkow@gmail.com>
2026-08-03 16:10:26 -0500

    Added RootScript and basic data types .

---
+++ RootScript.gd
@@ -0,0 +1,13 @@
+extends Node
+
+enum GAME_STATE {ON_ROAD, IN_CAR, IN_ENCOUNTER, MENU}
+var state: GAME_STATE
+
+var _player: Player
+var _inv: Inventory
+
+func _ready() -> void:
+	state = GAME_STATE.ON_ROAD
+	
+func _main() -> void:
+	pass
