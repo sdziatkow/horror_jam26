973bd8b1004c89942b7eee66994d2066d05cf741
Caden Pink <cadenpink@gmail.com>
2026-08-09 16:10:47 -0500

    zombie-lone-ranger

--- world_gen/Road.gd
+++ world_gen/Road.gd
@@ -2,74 +2,126 @@ class_name Road
 extends Node2D
 
 
-var _player : Player
 
-var _zombie_spawners : Array[ZombieSpawner]
 
+
+
+## Every tick, decisions about world generation are made 
 var _TICK_LEN : float = 1
+var _tick_timer : Timer = Timer.new()
+
+## The world is made of segments. Right now, a segment is the size of the road sprite
 var _BACK_SEGMENTS : int = 1
 var _TOTAL_SEGMENTS : int = 4
 
-var _tick_timer : Timer = Timer.new()
-
 var _segment_width : int
 
-var _move_weight : float = 0
+## When the move percentage reaches 100 percent (1.00), 
+## everything on the map moves back (like things placed on a treadmill)
+var _move_percentage : float = 0.0
+
+## Each segment will have it's own zombie spawner.
+var _zombie_spawner_scene = preload("res://world_gen/ZombieSpawner.tscn")
+var _zombie_spawners : Array[ZombieSpawner]
+
+## This is some space on the top and bottom of the map where zombies won't spawn
+var _SPAWN_VERTICAL_MARGIN : int = 100
+
+## The world needs to move the player back periodically
+var _player : Player
+
+
 
 func _ready() -> void:
 	
+	## Set up the tick timer
 	add_child(_tick_timer)
 	_tick_timer.one_shot = false
 	_tick_timer.timeout.connect(_tick_process)
 	
-	_segment_width = $Parallax2D/RoadSprite.texture.get_width()
+	##TODO: I use the sprite_size in _treadmill_setup. Maybe refactor later.
+	var sprite_size = $Parallax2D/RoadSprite.texture.get_size()
+	var sprite_pos = $Parallax2D/RoadSprite.position
+	_segment_width = sprite_size.x
 	
-	$LeftBoundary.position.x = -_segment_width
-
-
+	##Setting player movement boundaries 
+	$UpperBoundary.position.y = 0
+	$LowerBoundary.position.y = sprite_size.y
+	$LeftBoundary.position.x = -sprite_size.x
+	
+	## The spawners are reused. Once it reaches the end, it loops around
+	## Just like if you painted a dot on a treadmill.
+	_treadmill_setup()
+	
 func _treadmill_setup() -> void:
+	## Setting up all the spawners
 	for i in range(_TOTAL_SEGMENTS):
-		var new_spawner = ZombieSpawner.new()
-		new_spawner.set_spawn_rect($Parallax2D/RoadSprite.get_rect())
-		new_spawner.set_spawn_conditions(ZombieSpawner.SPAWN_MODES.LONE_RANGER, 1)
+		var new_spawner : ZombieSpawner = _zombie_spawner_scene.instantiate()
+		add_child(new_spawner)
 		_zombie_spawners.append(new_spawner)
+		
+		## Set spawn rectangle using width: road size height : road size - 2 * vertical margin 
+		var sprite_size = $Parallax2D/RoadSprite.texture.get_size()
+		new_spawner.set_spawn_rect(Rect2(0, _SPAWN_VERTICAL_MARGIN, 
+			sprite_size.x, sprite_size.y - 2 * _SPAWN_VERTICAL_MARGIN))
+		
+		## Only spawn condition I made right now, just set it now
+		new_spawner.set_spawn_conditions(ZombieSpawner.SPAWN_MODES.LONE_RANGER, 1)
+		new_spawner.preset_spawn()
+		
+	## Placing spawners
+	for i in range (- _BACK_SEGMENTS, _TOTAL_SEGMENTS - _BACK_SEGMENTS):
+		_zombie_spawners[i + _BACK_SEGMENTS].position.x = - i * _segment_width
+		_zombie_spawners[i + _BACK_SEGMENTS].position.y = 0
 	
 
 func give_camera(camera : Camera2D) -> void:
+	## setting the camera boundaries equal to the player movement boundaires.
+	## Might have to refactor this later if we add decorations around the map.
 	camera.limit_top = $UpperBoundary.position.y
 	camera.limit_bottom = $LowerBoundary.position.y
 	camera.limit_left = -_segment_width
 
-
+##TODO: I don't want the world to own the player
 func give_player(player : Player) -> void:
 	add_child(player)
 	_player = player
 	_player.position = Vector2(200, 200)
 
+## Starts the tick timer, generation, world movement, etc.
 func start_treadmill() -> void:
 	_tick_timer.start(_TICK_LEN)
 	
 func stop_treadmill() -> void:
 	_tick_timer.stop()
 	
+## This is the big one
 func _tick_process() -> void:
+	## Everything is decided by the player's position
 	var pos = _player.global_position.x
 	
-	if pos <= -.5 * _segment_width:
-		_move_weight -= .1
-		
-	elif pos >= 1.5 * _segment_width:
-		_move_weight += .3
-	elif pos >= _segment_width:
-		_move_weight += .2
+	## The move percentages are normalized by the tick length. Faster ticks -> less movement
 	
-	if _move_weight < 0:
-		_move_weight = 0
-	elif _move_weight >= 1:
-		_move_weight = 0
+	## If you're at the back of the map, decrease the move percentage 10%
+	if pos <= -.5 * _segment_width:
+		_move_percentage -= .1 * _TICK_LEN
+		
+	## If you're far into the second segment, or beyond, increase the move percentage 30%
+	elif pos >= 1.5 * _segment_width:
+		_move_percentage += .3 * _TICK_LEN
+	## If you're in the start of the second segment, increase the move percentage 20%
+	elif pos >= _segment_width:
+		_move_percentage += .2 * _TICK_LEN
+	
+	## Clamping the value at zero and then deciding if to move back everything
+	if _move_percentage < 0:
+		_move_percentage = 0
+	elif _move_percentage >= 1:
+		_move_percentage = 0
 		_move_back()
 
+## Moves all the segments back by one segment length
 func _move_back():
 	for node in get_children():
-		if node is Player:
+		if node is Player or node is ZombieSpawner:
 			node.position.x -= _segment_width
--- world_gen/ZombieSpawner.gd
+++ world_gen/ZombieSpawner.gd
@@ -5,12 +5,13 @@ enum SPAWN_MODES {VERTICAL_NORMAL, HORIZONTAL_NORMAL, CENTER_NORMAL, UNIFORM, LO
 
 var _spawned_zombies : Array[Zombie]
 var _zombie_pool : Array[Zombie] 
+var _zombie_scene : PackedScene = preload("res://enemies/Zombie.tscn")
 
 var _preset_spawn_mode : SPAWN_MODES 
 var _preset_spawn_count_goal : int
 var _preset_normal_deviation : float
 
-var _spawn_rect : Rect2 
+var _spawn_rect : Rect2
 
 
 func set_spawn_rect(rect : Rect2) -> void:
@@ -21,7 +22,11 @@ func custom_spawn(spawn_mode : SPAWN_MODES, spawn_count_goal : int, normal_devia
 	return true
 	
 func preset_spawn() -> void:
-	pass
+	match _preset_spawn_mode:
+		SPAWN_MODES.LONE_RANGER:
+			_spawn_one(_spawn_rect.position.x + randi_range(0, _spawn_rect.size.x), 
+				_spawn_rect.position.y + randi_range(0, _spawn_rect.size.x))
+		 
 	
 func set_spawn_conditions(spawn_mode : SPAWN_MODES, spawn_count_goal : int, normal_deviation : int = 0) -> bool:
 	## TODO: check parameters 
@@ -29,6 +34,12 @@ func set_spawn_conditions(spawn_mode : SPAWN_MODES, spawn_count_goal : int, norm
 	_preset_spawn_count_goal = spawn_count_goal
 	_preset_normal_deviation = normal_deviation
 	return true
+
+func _spawn_one(x_pos_px : int, y_pos_px) -> void:
+	var new_zombie : Zombie = _zombie_scene.instantiate()
+	add_child(new_zombie)
+	new_zombie.z_index = 1
+	new_zombie.position = Vector2( x_pos_px, y_pos_px)
 
 func despawn() -> void:
 	for zombie : Zombie in _spawned_zombies:
